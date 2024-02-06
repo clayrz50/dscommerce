d15f7e3a3c62b29664a3c048e06538900ff37917
Clayton <clayrz50@gmail.com>
2024-02-06 19:12:12 -0300

    Get User Logged

--- src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +17,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 			+ "FROM tb_user " + "INNER JOIN tb_user_role ON tb_user.id = tb_user_role.user_id "
 			+ "INNER JOIN tb_role ON tb_role.id = tb_user_role.role_id " + "WHERE tb_user.email = :email")
 	List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
+	
+	Optional<User> findByEmail(String email);
 }
